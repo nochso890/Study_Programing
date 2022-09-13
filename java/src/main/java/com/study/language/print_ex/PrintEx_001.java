a84571cbd1e6219b72ae3a6179cdc6d919c3c7ef
nochso <nochso890@gmail.com>
2022-09-13 14:19:15 +0900

    github config test

--- java/src/main/java/com/study/language/print_ex/PrintEx_001.java
+++ java/src/main/java/com/study/language/print_ex/PrintEx_001.java
@@ -1,21 +1,16 @@
 package com.study.language.print_ex;
 
-import java.util.logging.Logger;
 
 public class PrintEx_001 {
-
-    public static Logger logger = Logger.getGlobal();
 
     public static void main(String[] args) {
         byte b = 1;
         short s = 2;
         char c = 'A';
-        String d = "AB";
 
         System.out.println("b : "+b);
         System.out.println("s : "+s);
         System.out.println("c : "+c);
-        System.out.println("d : "+d);
 
     }
 
