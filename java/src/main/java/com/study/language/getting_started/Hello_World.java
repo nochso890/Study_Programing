ba0c17730f0ec11be4e9a7e83acb93914e3ab8ac
nochso <nochso890@gmail.com>
2022-09-22 10:57:36 +0900

    hello world test

---
+++ java/src/main/java/com/study/language/getting_started/Hello_World.java
@@ -0,0 +1,27 @@
+package com.study.language.getting_started;
+
+public class Hello_World {
+
+    /**
+     * 1. 자바 Java Programming Language
+     * <p>1-1 자바란?</p>
+     * Sun Microsystems 에서 개발한 객체지향 프로그래밍 언어.
+     * 자바는 JVM 으로 인해 운영체제에 독립적이다.
+     *
+     * <p>1-2 자바의 역사</p>
+     * Oak(오크)라는 언어에서 부터 시작됨.
+     * 제임스 고슬링과 아서 벤 호프 와 같은 썬의 엔지니어가 가전제품의 탑제 목적으로 만듬.
+     *
+     * <p>1-3 자바언어의 특징</p>
+     *
+     * <b>1. 운영체제에 독립적이다.</b>
+     *
+     * <b>2. 객체지향언어다.</b>
+     *
+     * */
+
+    public void test(){
+        System.out.println("Hello!!!");
+    }
+
+}
