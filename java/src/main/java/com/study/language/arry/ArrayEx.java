6f5602796fbaef02dccf4b43513b5783c7d85a08
nochso <nochso890@gmail.com>
2022-10-04 22:41:56 +0900

    2022-10-04 Java Study Commit

--- java/src/main/java/com/study/language/arry/ArrayEx.java
+++ java/src/main/java/com/study/language/arry/ArrayEx.java
@@ -11,6 +11,8 @@ public class ArrayEx {
 
         System.out.println("getNumberArrayLength : " + arrayEx.getNumberArrayLength(arrayNumber));
         System.out.println("getStrArrayLength : " + arrayEx.getStrArrayLength(arrayStr));
+        arrayEx.arrayCopy();
+        arrayEx.shuffleArray();
 
     }
 
@@ -19,6 +21,41 @@ public class ArrayEx {
         for (int i = 0; i < arrayNumber.length; i++) {
             System.out.println("Index :" + i);
             System.out.println(i + "번째 값 : " + arrayNumber[i]);
+        }
+
+    }
+
+    private void arrayCopy(){
+        char[] abc = {'A','B','C','D'};
+        char[] num = {'0','1','2','3','4','5'};
+        char[] tmp = new char[abc.length + num.length];
+
+        System.arraycopy(abc,0,tmp,0,abc.length);
+        System.arraycopy(num,0,tmp,abc.length,num.length);
+        System.out.println(tmp);
+    }
+
+    private void shuffleArray(){
+
+        int[] numArray = new int[10];
+
+        for(int i=0; i < numArray.length; i++){
+            numArray[i] = i;
+            System.out.println(numArray[i]);
+        }
+
+        System.out.println("=================");
+
+        for(int i=0; i < 11; i++){
+            int n = (int) (Math.random() * 10);
+            System.out.println("N : "+ n);
+            int tmp = numArray[0];
+            numArray[0] = numArray[n];
+            numArray[n] = tmp;
+        }
+
+        for(int i=0; i < numArray.length; i++){
+            System.out.println(numArray[i]);
         }
 
     }
